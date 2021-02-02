76ab766bb8454e99942e74ad4ca7bb3b64af9237
EmreKLC414 <77151060+EmreKLC414@users.noreply.github.com>
2021-02-02 22:18:41 +0300

    Update autostar.py

--- auto-react/autostar.py
+++ auto-react/autostar.py
@@ -1,7 +1,6 @@
 import asyncio
 import discord
 
-   @commands.command()
    async def on_message(message):
      if(message.channel.id == "775039612053094420"):
      await client.add_reaction(message, "⭐")
