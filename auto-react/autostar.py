0c9e1ce1d88a8e60756a9de07b77d4908a6120bd
EmreKLC414 <77151060+EmreKLC414@users.noreply.github.com>
2021-02-02 19:28:45 +0300

    Update autostar.py

--- auto-react/autostar.py
+++ auto-react/autostar.py
@@ -1,7 +1,6 @@
-import discord
-from discord.ext    import commands
-from discord.ext.commands   import Bot
 import asyncio
+import discord
+from discord.ext
 
 class Autoreact(commands.Cog):
      def __init__(self, bot):
