6165c86bd7aa67d4e6ad337f05aaf24bdb0c965a
EmreKLC414 <77151060+EmreKLC414@users.noreply.github.com>
2021-02-02 23:44:09 +0300

    Update auto-react.py

--- auto-react/auto-react.py
+++ auto-react/auto-react.py
@@ -3,8 +3,7 @@ import discord
 from discord.ext import commands
 
 
-
-
+@bot.listen()
 async def on_message(message):
     if(message.channel.id == "802867247772074004"):
         await client.add_reaction(message, "⭐")
