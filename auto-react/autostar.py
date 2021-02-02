05e8710a9736b2b0ea02cf9e38d97376a23c79ed
EmreKLC414 <77151060+EmreKLC414@users.noreply.github.com>
2021-02-02 19:50:49 +0300

    Update autostar.py

--- auto-react/autostar.py
+++ auto-react/autostar.py
@@ -1,12 +1,18 @@
 import asyncio
-from discord.ext
+import emoji
+
+import discord
+from discord.ext import commands
 
 class Autoreact(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.db = bot.api.get_plugin_partition(self)
 
 	@client.event
-	async def on_message(message):
- 		if(message.channel.id == "775039612053094420"):
-		await client.add_reaction(message, "⭐")
+   async def on_message(message):
+     if(message.channel.id == "775039612053094420"):
+     await client.add_reaction(message, "⭐")
 		
 		
 def setup(bot):
