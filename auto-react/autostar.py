3dbd18cd1b62b96362389cd7074aa03875f351a9
EmreKLC414 <77151060+EmreKLC414@users.noreply.github.com>
2021-02-02 22:06:11 +0300

    Update autostar.py

--- auto-react/autostar.py
+++ auto-react/autostar.py
@@ -1,11 +1,9 @@
 import asyncio
 import emoji
-from core import checks
 import discord
-from discord.ext import commands
+from discord.ext
 
 
-   @client.event
    async def on_message(message):
      if(message.channel.id == "775039612053094420"):
      await client.add_reaction(message, "⭐")
