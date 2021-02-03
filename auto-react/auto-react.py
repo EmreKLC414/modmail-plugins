d655b7249fbb3339dd0eab8927fd308f93ef58c7
EmreKLC414 <77151060+EmreKLC414@users.noreply.github.com>
2021-02-03 14:23:24 +0300

    Update auto-react.py

--- auto-react/auto-react.py
+++ auto-react/auto-react.py
@@ -7,7 +7,7 @@ class Autoreact(commands.Cog):
     self.bot = bot
 
 @commands.command
-async def on_message(self, message):
+async def on_message(self, ctx):
     if(message.bot.get_channel(802867247772074004)):
         await ctx.message.add_reaction("⭐")
 	
