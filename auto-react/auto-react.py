aead72bdf6ecbc8fdfa1decd00dc59637e66e238
EmreKLC414 <77151060+EmreKLC414@users.noreply.github.com>
2021-02-03 15:36:20 +0300

    Update auto-react.py

--- auto-react/auto-react.py
+++ auto-react/auto-react.py
@@ -2,15 +2,15 @@ import discord
 from discord.ext import commands
 
 class Autoreact(commands.Cog):
-  def __init__(self, bot):
-    self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-@commands.Cog.listener()
-async def on_message(self, message):
-    if message.author.bot:
-        return
-    print("Hello.")
-	
-		
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+        print("Hello.")
+    
+        
 def setup(bot):
     bot.add_cog(Autoreact(bot))
