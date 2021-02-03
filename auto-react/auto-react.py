f64d57a9f983a0a516c67ae057fd5512a7346096
EmreKLC414 <77151060+EmreKLC414@users.noreply.github.com>
2021-02-03 14:45:21 +0300

    Update auto-react.py

--- auto-react/auto-react.py
+++ auto-react/auto-react.py
@@ -5,7 +5,7 @@ class Autoreact(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
 
-@commands.command()
+@commands.Cog.listener()
 async def on_message(self, message):
     if message.author.bot:
         return
