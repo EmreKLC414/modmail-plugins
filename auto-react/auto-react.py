7e18f56291d192990622c0246ebcfb035423c020
EmreKLC414 <77151060+EmreKLC414@users.noreply.github.com>
2021-02-03 16:09:54 +0300

    Update auto-react.py

--- auto-react/auto-react.py
+++ auto-react/auto-react.py
@@ -18,14 +18,17 @@ class Autoreact(commands.Cog):
 def setup(bot):
     bot.add_cog(Autoreact(bot))
 
-    
-    
+ 
+
+# Examples for multiple usage
+#
+#   
 # For Multiple Channel;
 # if message.channel.id in [channel_id_one, channel_id_two, channel_id_three]:
 #    await message.add_reaction("emojihere")
 #
 #
 #For Multiple Emoji
-#        if message.channel.id == 802867247772074004: #Channel_ID
+#        if message.channel.id == Channel_ID:
 #            await message.add_reaction("emojihere")
 #            await message.add_reaction("emojihere")
