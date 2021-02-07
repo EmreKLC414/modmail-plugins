b080547b82cc3c93c9daaa044852fd2cacda6bb4
EmreKLC414 <77151060+EmreKLC414@users.noreply.github.com>
2021-02-07 19:18:05 +0300

    Update publisher.py

--- publisher/publisher.py
+++ publisher/publisher.py
@@ -4,18 +4,16 @@ from discord.ext import commands
 from core import checks
 from core.models import PermissionLevel
 
-class Publish(commands.Cog): 
-    """Publish messages sent in announcement channels"""
     
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    @checks.has_permissions(PermissionLevel.MODERATOR)
-    async def publish(self, ctx, message_id: discord.Message):
+    @commands.Cog.listener()
+    async def publish(self, message.channel.id: discord.Message):
         """Publish message sent in announcement channel."""
-        await message_id.publish()
-        await ctx.send("Published message successfully.")
+        if message.channel.id == 802867247772074004:
+            await message.publish()
+            
                                         
 def setup(bot):
     bot.add_cog(Publish(bot))
