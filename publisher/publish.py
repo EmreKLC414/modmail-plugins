76be952587f8af7244ccb34f4010e1423f55ffad
EmreKLC414 <77151060+EmreKLC414@users.noreply.github.com>
2021-02-07 19:04:03 +0300

    Update publish.py

--- publisher/publish.py
+++ publisher/publish.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from core import checks
 from core.models
 
-class Autoreact(commands.Cog):
+class Publish(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
