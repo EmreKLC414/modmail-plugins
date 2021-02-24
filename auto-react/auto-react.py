eadfe173fa2c53190bd0c5c2bd00d54c3a89f240
EmreKLC414 <77151060+EmreKLC414@users.noreply.github.com>
2021-02-24 17:57:18 +0300

    Update auto-react.py

--- auto-react/auto-react.py
+++ auto-react/auto-react.py
@@ -13,6 +13,9 @@ class Autoreact(commands.Cog):
             return
         if message.channel.id == 775039612053094420: #Channel_ID
             await message.add_reaction("\u2b50")  # Emoji
+        if message.channel.id == 814142601145221160:
+            await message.add_reaction("<:iyi:809187585913126912>")
+            await message.add_reaction("<:gg:809187585779433496>")
     
         
 def setup(bot):
